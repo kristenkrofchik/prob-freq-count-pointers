3e3a055b5d659c0a3dc2452425d84071fcfb9957
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-04 05:51:01 -0500

    set up testing

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+// accepts two strings (all lowercase, no space or special char): message and letters
+// should return true if the message can be built with letters; otherwise should return false
+//constraint: O(M + L) where M is the length of message and N is the length of letters
+
+
+function constructNote(message, letters) {
+    let messageChars = makeFreqCounter(message);
+    let lettersChars = makeFreqCounter(letters);
+
+    for(let char of messageChars) {
+        if(lettersChars[char] >= messageChars[char]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function makeFreqCounter(str) {
+    let freqCounter = {};
+    for(let char of str) {
+        freqCounter[char] = (freqCounter[char] + 1) || 1;
+    }
+    return freqCounter;
 }
