5d9001571c467e497bfaef1b08ff7525da3672d7
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-07 16:48:21 -0500

    added comments for construct-note

--- construct-note.js
+++ construct-note.js
@@ -3,22 +3,26 @@
 //constraint: O(M + L) where M is the length of message and N is the length of letters
 
 function constructNote(message, letters) {
+    //use helper function makeFreqCounter() to create freq counters for message and letters
     let messageChars = makeFreqCounter(message);
     let lettersChars = makeFreqCounter(letters);
 
+    //if the message is empty, return true
     if(message === '') {
         return true;
     }
 
+    //if the count for a char in letters is greater than or equal to the count for the same char in message, return true
     for(let char of message) {
         if(lettersChars[char] >= messageChars[char]) {
             return true;
         }
     }
+    //else return false
     return false;
 }
 
-//helper function to make a frequency counter
+//helper function to make a frequency counter from a string
 function makeFreqCounter(str) {
     let freqCounter = {};
     for(let char of str) {
