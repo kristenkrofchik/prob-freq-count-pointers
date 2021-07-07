f7d24ad62b05c72f60a1637ccb6704b62d0c3135
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-07 17:15:57 -0500

    working on two-array-objects

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+//function accepts 2 arrays: one of keys and one of values
+//function should return and object created from the keys and values.
+//if there are not enough values, the rest of the keys will have a value of null. iI there are not enough keys, the rest of the values will be ignored. 
+
+function twoArrayObject(keys, values) {
+    let object = {};
+
+    for(let i = 0; i < keys.length; i++) {
+        if(keys.length === values.length) {
+            object[key[i]] === values[i] || null;
+    }
 }
