f8a180a2c16f65adcf88da820c29be7a37eaf522
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-07 21:55:58 -0500

    added comments two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -3,11 +3,14 @@
 //if there are not enough values, the rest of the keys will have a value of null. iI there are not enough keys, the rest of the values will be ignored. 
 
 function twoArrayObject(keys, values) {
+    //create a new empty object
     let newObject = {};
 
     for(let i = 0; i < keys.length; i++) {
+        //if the index is less than the length of values, use the value at that index
         if(i < values.length) {
             newObject[keys[i]] = values[i];
+        //if the index is greater than the length of values, use null
         } else {
             newObject[keys[i]] = null;
         }
