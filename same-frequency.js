c758e738885cffecea6c118c4e6eac686f2f8476
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-07 22:03:00 -0500

    added comments to same-frequency

--- same-frequency.js
+++ same-frequency.js
@@ -2,17 +2,21 @@
 //returns true if the two numbers have the same frequency of digits, otherwise returns false
 
 function sameFrequency(int1, int2) {
+    //create frequency counters for the given integers
     let int1Counter = makeFreqCounter(int1);
     let int2Counter = makeFreqCounter(int2);
 
+    //if the frequncy of any digit in int1 does not match the frequency in int2, return false
     for(char of int1.toString()) {
         if(int1Counter[char] != int2Counter[char]) {
             return false;
         }
     }
+    //else, return true
     return true;
 }
 
+//helper function to make a frequency counter from a number
 function makeFreqCounter(num) {
     let freqCounter = {};
 
