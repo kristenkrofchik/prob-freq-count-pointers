3100ed70cd2110a5f68fcee8d6228ff944753d4a
Kristen Krofchik <k.krofchik@gmail.com>
2021-07-07 22:01:31 -0500

    passing tests for same-frequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+// function accepts two positive integers
+//returns true if the two numbers have the same frequency of digits, otherwise returns false
+
+function sameFrequency(int1, int2) {
+    let int1Counter = makeFreqCounter(int1);
+    let int2Counter = makeFreqCounter(int2);
+
+    for(char of int1.toString()) {
+        if(int1Counter[char] != int2Counter[char]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function makeFreqCounter(num) {
+    let freqCounter = {};
+
+    let numString = num.toString();
+
+    for(char of numString) {
+        freqCounter[char] = (freqCounter[char] + 1) || 1;
+    }
+
+    return freqCounter;
 }
